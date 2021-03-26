c0ba5696d5fe6f0e5c7ab2789f41702425a63a9f
Takeyuki UEDA <ueda@latelierdueda.com>
2021-03-26 19:11:11 +0900

    first commit

---
+++ dev/serve.vue
@@ -0,0 +1,23 @@
+<script>
+import Vue from 'vue';
+//import { FccButton } from "@/entry";
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { VueUserFeSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  //components: {
+  //  FccButton
+  //}
+  // components: {
+  //  VueUserFeSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+<!--    <vue-user-fe-sample /> -->
+    <FccButton />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,3 @@
+/* eslint-disable import/prefer-default-export */
+export { default as VueUserFeSample } from './vue-user-fe-sample.vue';
+export { default as FccButton } from './FccButton';
